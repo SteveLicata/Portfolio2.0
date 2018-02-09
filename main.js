684db95baf230edc0d471cbf63e81f0d35044657
Steve Licata <Steve.Licata@gmail.com>
2018-02-08 22:02:05 -0500

    scroll reveal js update

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
 window.onload = function(){
   console.log("DOM Loaded");
 
-  window.sr = ScrollReveal({ reset: true, mobile: true, rotate: { x: 50, y: 50, z: 50 }, });
 
+  // SCROLL REVEAL
+  window.sr = ScrollReveal({ reset: true, mobile: true, rotate: { x: 0, y: 0, z: 0 }, });
+
+    sr.reveal('.mystoryblurb');
     sr.reveal('.mystorysummary');
     sr.reveal('.mystorytext');
+    sr.reveal('.toplinks');
 
-
- // Scroll Reveal Attribution:
- // https://github.com/jlmakes/scrollreveal
- // https://scrollrevealjs.org/
-
+    // Scroll Reveal Attribution:
+  // https://github.com/jlmakes/scrollreveal
+  // https://scrollrevealjs.org/
 
 
 // end window.onload
