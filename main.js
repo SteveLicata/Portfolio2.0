2fc51fe5c892898e3afd02a1e1538943d2b877bd
Steve Licata <Steve.Licata@gmail.com>
2018-02-08 21:39:21 -0500

    google text

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
 window.onload = function(){
   console.log("DOM Loaded");
 
+  window.sr = ScrollReveal({ reset: true, mobile: true });
+
+    sr.reveal('.mystorysummary', rotate: { x: 10, y: 20, z: 30});
+
+
+
+ });
+
+
+
+ // Scroll Reveal Attribution:
+ // https://github.com/jlmakes/scrollreveal
+ // https://scrollrevealjs.org/
+
+
+
 // end window.onload
 };
